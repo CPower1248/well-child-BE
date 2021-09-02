1e330347c69d97da1a032e110387319b786b8e44
corey-power <CPower1248@gmail.com>
2021-09-01 21:18:25 -0400

    ...

--- data/migrations/20210901032345_content.js
+++ data/migrations/20210901032345_content.js
@@ -1,7 +1,12 @@
 exports.up = function(knex) {
-  return knex.schema.createTable("content", tbl => {
+  return knex.schema
+    .createTable("content", tbl => {
     tbl.increments()
     tbl.string("content").notNullable()
+  })
+
+  .createTable("age", tbl => {
+    tbl.increments()
   })
 };
 
