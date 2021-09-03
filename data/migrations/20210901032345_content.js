4fcb8b48c1c6ee3d1a0b4cc401518ad19ba21574
corey-power <CPower1248@gmail.com>
2021-09-02 20:08:02 -0400

    Created age/category/content tables

--- data/migrations/20210901032345_content.js
+++ data/migrations/20210901032345_content.js
@@ -1,13 +1,33 @@
 exports.up = function(knex) {
   return knex.schema
-    .createTable("content", tbl => {
-    tbl.increments()
-    tbl.string("content").notNullable()
-  })
+    .createTable("age", tbl => {
+      tbl.increments()
+      tbl.integer("age").notNullable().unique()
+    })
 
-  .createTable("age", tbl => {
-    tbl.increments()
-  })
+    .createTable("category", tbl => {
+      tbl.increments()
+      tbl.string("category").notNullable().unique()
+    })
+
+    .createTable("content", tbl => {
+      tbl.increments()
+      tbl.string("content").notNullable()
+      tbl.integer("age_id")
+        .references("id")
+        .inTable("age")
+        .notNullable()
+        .unsigned()
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE')
+      tbl.integer("category_id")
+        .references("id")
+        .inTable("category")
+        .notNullable()
+        .unsigned()
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE')
+    })
 };
 
 exports.down = function(knex) {
